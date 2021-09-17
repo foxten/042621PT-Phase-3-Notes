137f049698e68788a9c2908add6503796604df97
Tenaysia Fox <tenaysiafox@flatironschool.com>
2021-09-17 10:38:40 -0500

    091621 Notes w/ weekend prompts!

--- intro_to_classes.rb
+++ intro_to_classes.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
-# music as our example, starting with Albums
+# music as our example, starting with Albums (x)
+# store all instances of the Album class (x)
+# create private methods to update the list of songs on an album, and to store the copyright info (x)
+# create subclasses for digital vs physical releases (x)
 
-# store all instances of the Album class
-    # return only the artist names 
-
-# create private methods to update the list of songs on an album, and to store the copyright info
-
+# if our artist wanted to release an album, merch, and event tickets/info, what data could we store in a separate module?
 
 class Album
-    attr_accessor :album_title, :artist_name, :album_cover, :album_genre, :album_length, :songs
+    # renamed attributes so program can call them without confusing the attribute with the instance method
+    attr_accessor :album_title, :artist_name, :cover, :genre, :length, :songs
     
     @@all = []
 
@@ -19,17 +19,16 @@ class Album
         @@all << self
     end    
  
-    def album_cover album
-        self.album_cover = album
+    def album_cover art
+        self.cover = album
     end
    
     def album_genre genre
-        self.album_genre = genre
+        self.genre = genre
     end
     
     def album_length length
-        self.album_length = length
-        tracklist([])
+        self.length = length
     end
 
     def self.all 
@@ -46,51 +45,21 @@ class Album
 
 end 
 
+class Digital < Album
+    # streaming services
+    attr_accessor :services
+    def initialize (title, artist, streaming_services)
+        super(title, artist)
+        @services = []
+        @services << streaming_services
+    end
+
+end
+
+class Physical < Album
+    # availability 
+
+end
+
+
 binding.pry
-
-
-# # title
-# def album_title=(title)
-#     @album_title = title 
-# end
-
-# def album_title
-#     @album_title
-# end
-
-# # artist
-# def artist_name=(artist)
-#     @artist_name = artist
-# end 
-
-# def artist_name
-#     @artist_name
-# end
-
-# # album cover
-# def album_cover=(album_art)
-#     @album_cover = album_art
-# end
-
-# def album_cover
-#     @album_cover 
-# end
-
-# # genre
-# def album_genre=(genre)
-#     @album_genre = genre
-# end
-
-# def album_genre
-#     @album_genre
-# end
-
-# # length 
-# def album_length=(length)
-#     @album_length = length
-# end
-
-# def album_length
-#     @album_length 
-# end
-
